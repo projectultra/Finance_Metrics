ebc81da920bd017374ad258bfc98cf042fc4613d
Jonas Ferrao <jonaschris2103@gmail.com>
2023-05-13 07:48:40 +0000

    allowed hosts

--- FM/settings.py
+++ FM/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['financemetrics.azurewebsites.net']
 
 
 # Application definition

787001dc2509cce95345e83c6f427e765b8c3e73
Jonas Ferrao <jonaschris2103@gmail.com>
2023-05-26 04:43:13 +0000

    unit tests

--- FinanceMetrics/tests.py
+++ FinanceMetrics/tests.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 import requests
 import django
-from FinanceMetrics.models import EconomicIndicators
+#from FinanceMetrics.models import EconomicIndicators
 from FinanceMetrics.models import METAstock
 from FinanceMetrics.models import AAPLstock
 from FinanceMetrics.models import AMZNstock
@@ -19,9 +19,12 @@ from FinanceMetrics.models import MSFTstock
 from FinanceMetrics.models import TSLAstock
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FM.settings')
 django.setup()
-# Create your views here.
-
+#from django.conf import settings
+#settings.configure()
 prevdate=pd.read_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv')['LastDate']
+interest_rate=0.0
+inflation_rate=0.0
+currency_rate=0.0
 def DisplayStock():
     tdapi_key=os.environ.get('TWELVEDATAAPI_KEY')
     if((datetime.datetime.date(datetime.datetime.today()))!=(pd.to_datetime(prevdate).dt.date[0])):
@@ -34,42 +37,47 @@ def DisplayStock():
         FetchTSLAStock(tdapi_key)
         FetchMSFTStock(tdapi_key)
         storeprices(datetime.datetime.today())
-    else:
-        fetchprices()
 def FetchMETAstock(tdapi_key):
     url='https://api.twelvedata.com/time_series?symbol=META&interval=1day&outputsize=7&format=CSV&apikey='+tdapi_key
     response = requests.get(url)
     METAstock.predicted_price=Predictstock(METAstock,conversion(response))
+    print('Meta:',METAstock.predicted_price)
 
 def FetchAAPLStock(tdapi_key):
     url='https://api.twelvedata.com/time_series?symbol=AAPL&interval=1day&outputsize=7&format=CSV&apikey='+tdapi_key
     response = requests.get(url)
     AAPLstock.predicted_price=Predictstock(AAPLstock,conversion(response))
+    print('Apple:',AAPLstock.predicted_price)
 
 def FetchAMZNStock(tdapi_key):
     url='https://api.twelvedata.com/time_series?symbol=AMZN&interval=1day&outputsize=7&format=CSV&apikey='+tdapi_key
     response = requests.get(url)
     AMZNstock.predicted_price=Predictstock(AMZNstock,conversion(response))
+    print('Amazon:',AMZNstock.predicted_price)
 
 def FetchNFLXStock(tdapi_key):
     url='https://api.twelvedata.com/time_series?symbol=NFLX&interval=1day&outputsize=7&format=CSV&apikey='+tdapi_key
     response = requests.get(url)
     NFLXstock.predicted_price=Predictstock(NFLXstock,conversion(response))
+    print('Netflix:',NFLXstock.predicted_price)
 
 def FetchGOOGStock(tdapi_key):
     url='https://api.twelvedata.com/time_series?symbol=GOOG&interval=1day&outputsize=7&format=CSV&apikey='+tdapi_key
     response = requests.get(url)
     GOOGstock.predicted_price=Predictstock(GOOGstock,conversion(response))
+    print('Google:',GOOGstock.predicted_price)
 
 def FetchMSFTStock(tdapi_key):
     url='https://api.twelvedata.com/time_series?symbol=MSFT&interval=1day&outputsize=7&format=CSV&apikey='+tdapi_key
     response = requests.get(url)
     MSFTstock.predicted_price=Predictstock(MSFTstock,conversion(response))
+    print('Microsoft:',MSFTstock.predicted_price)
 
 def FetchTSLAStock(tdapi_key):
     url='https://api.twelvedata.com/time_series?symbol=TSLA&interval=1day&outputsize=7&format=CSV&apikey='+tdapi_key
     response = requests.get(url)
     TSLAstock.predicted_price=Predictstock(TSLAstock,conversion(response))
+    print('Tesla:',TSLAstock.predicted_price)
 
 def FetchEconomicIndicators():
     api_key=os.environ.get('API_KEY')
@@ -77,16 +85,16 @@ def FetchEconomicIndicators():
 
     url = 'https://api.api-ninjas.com/v1/inflation?country={}'.format(country)
     r = requests.get(url, headers={'X-Api-Key': os.environ.get('NINJAAPI_KEY')})
-    EconomicIndicators.inflation = json.loads(r.text)[0]['yearly_rate_pct']
+    print(json.loads(r.text)[0]['yearly_rate_pct'])
 
     url = 'https://www.alphavantage.co/query?function=FEDERAL_FUNDS_RATE&interval=daily&datatype=csv&apikey='+api_key
     r = requests.get(url)
     lines = r.text.strip().split('\n')
-    EconomicIndicators.interest_rate = lines[1].split(',')[1]
+    print(lines[1].split(',')[1])
 
     url = 'https://api.api-ninjas.com/v1/convertcurrency?want=GBP&have=USD&amount=1'
     r = requests.get(url, headers={'X-Api-Key': os.environ.get('NINJAAPI_KEY')})
-    EconomicIndicators.currency = json.loads(r.text)['new_amount']
+    print(json.loads(r.text)['new_amount'])
 
 def Predictstock(Stock,data_array):
 
@@ -125,15 +133,6 @@ def storeprices(lastdate):
     PredictedStock = pd.concat([PredictedStock], ignore_index=True)
     PredictedStock.to_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv',index=False)
 
-def fetchprices():
-    AAPLstock.predicted_price=pd.read_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv')['Apple'][0]
-    AMZNstock.predicted_price=pd.read_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv')['Amazon'][0]
-    METAstock.predicted_price=pd.read_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv')['Meta'][0]
-    NFLXstock.predicted_price=pd.read_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv')['Netflix'][0]
-    GOOGstock.predicted_price=pd.read_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv')['Google'][0]
-    TSLAstock.predicted_price=pd.read_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv')['Tesla'][0]
-    MSFTstock.predicted_price=pd.read_csv(r'FinanceMetrics/LivePrices/PredictedStock.csv')['Microsoft'][0]
-
 def conversion(response):
     lines = response.text.split("\n")[1:-1]
     data_rows = []
@@ -146,9 +145,9 @@ def conversion(response):
             float(values[2]),  # high
             float(values[3]),  # low
             float(values[4]),  # close
-            float(EconomicIndicators.interest_rate),
-            float(EconomicIndicators.currency),
-            float(EconomicIndicators.inflation),
+            float(interest_rate),
+            float(currency_rate),
+            float(inflation_rate),
             int(date.day),
             int(date.month),
         ]

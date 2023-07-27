39b066fac53d2519ea1008a05ecb38a921bacb09
projectultra <jonaschris2103@gmail.com>
2023-07-27 20:30:23 +0530

    allowed hosts

--- FM/settings.py
+++ FM/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'financemetrics.onrender.com']
 
 
 # Application definition

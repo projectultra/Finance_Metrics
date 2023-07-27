1c0c4dd9e160b581b1e7c0ad671c3f8a7944a92b
projectultra <jonaschris2103@gmail.com>
2023-07-27 12:43:45 +0530

    vercel

--- FM/settings.py
+++ FM/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 

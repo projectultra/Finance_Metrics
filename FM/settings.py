e2c2abba2872dd0f12948f6d8fa9bb9133107a4f
Jonas Ferrao <jonaschris2103@gmail.com>
2023-08-20 12:10:00 +0000

    Integrated With MongoDB Atlas

--- FM/settings.py
+++ FM/settings.py
@@ -22,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',
-                 'localhost',]
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -74,16 +73,6 @@ WSGI_APPLICATION = 'FM.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': 'FinanceMetrics',
-#         'ENFORCE_SCHEMA': False,
-#         'HOST': 'localhost',
-#         'PORT': 27017,
-#     }
-# }
 
 
 # Password validation
